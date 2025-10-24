9741bc19633074a2cc7dc513db1fb36b7a9f401c
Weihan-Xu-wxuu0047 <wxuu0047@student.monash.edu>
2025-10-24 13:36:27 +1100

    bug fixed

--- NoMash-Library/src/router/index.js
+++ NoMash-Library/src/router/index.js
@@ -12,7 +12,7 @@ import CountBookAPI from '../views/CountBookAPI.vue';
 
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes:[
     {
       path: '/',
